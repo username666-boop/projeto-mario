84f066feadaf3c6d248e821417dfc5d74e1de053
Zet <64151536+username666-boop@users.noreply.github.com>
2023-01-22 18:20:17 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+// variaveis
+const video = document.getElementById("video");
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
